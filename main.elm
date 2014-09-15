af58a539668f4e7e65b88475eab6dec3b6ce2142
William O'Hanley <liam@wohanley.com>
2014-09-16 11:06:53 +1200

    fix indentation

--- main.elm
+++ main.elm
@@ -7,7 +7,7 @@ main = renderGrid (generateGrid 30 30)
 
 generateGrid : Int -> Int -> Grid
 generateGrid rows cols =
-  repeat rows (repeat cols True)
+    repeat rows (repeat cols True)
 
 -- Display
 
@@ -34,33 +34,43 @@ step grid = indexedMap
 
 stepCell : Int -> Int -> Grid -> CellState
 stepCell row column grid =
-  (getNeighbours row column grid)
-    |> filter (\cell -> cell)
-    |> length
-    |> liveOrDie
+    (getNeighbours row column grid)
+        |> filter (\cell -> cell)
+        |> length
+        |> liveOrDie
 
-getNeighbours : Int -> Int -> Grid -> [CellState]
-getNeighbours row column grid = 
+getNeighbours : Int -> Int -> Int -> Int -> Grid -> [CellState]
+getNeighbours maxRow maxColumn row column grid =
+  if 
+
+range : Int -> Int -> [Int]
+range from to = tailRange from to []
+
+tailRange : Int -> Int -> [Int] -> [Int]
+tailRange from to list =
+    if from == to
+    then list
+    else from :: (tailRange (from + 1) to list)
 
 -- Hahahaha I actually have no idea how to get from a list
 get : Int -> [a] -> a
 get index xs =
-  if index == 0
-  then head xs
-  else get (index - 1) (tail xs)
+    if index == 0
+    then head xs
+    else get (index - 1) (tail xs)
 
 liveOrDie : Int -> CellState
 liveOrDie livingNeighbours =
-  case livingNeighbours of
-    2 -> True
-    3 -> True
-    _ -> False
+    case livingNeighbours of
+      2 -> True
+      3 -> True
+      _ -> False
 
 indexedMap : (Int -> a -> b) -> [a] -> [b]
 indexedMap f xs = tailIndexedMap f 0 xs
 
 tailIndexedMap : (Int -> a -> b) -> Int -> [a] -> [b]
 tailIndexedMap f fromIndex xs =
-  case xs of
-    [] -> []
-    head::tail -> (f fromIndex head) :: (tailIndexedMap f (fromIndex + 1) tail)
+    case xs of
+      [] -> []
+      head::tail -> (f fromIndex head) :: (tailIndexedMap f (fromIndex + 1) tail)
